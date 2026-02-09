191270c5b6c188583f3f7b9a22cadd634d483545
Natanael Canche <jose_chingon@live.com.mx>
2026-02-08 23:07:46 -0600

    Add files via upload

---
+++ Pract 2-Gestion de memoria/Practica_MemoriaDinamica.py
@@ -0,0 +1,19 @@
+
+frutas = []
+
+
+frutas.append("Mango")   
+frutas.append("Manzana") 
+frutas.append("Banana")  
+frutas.append("Uvas")    
+
+print(f"Lista inicial: {frutas}")
+
+
+frutas.pop(0)    
+frutas.pop(1)   
+
+
+frutas.append("Sandia")
+
+print(f"Lista final: {frutas}")
---
+++ Pract 2-Gestion de memoria/Practica_MemoriaEstatica.py
@@ -0,0 +1,11 @@
+
+calificaciones = [] 
+
+for i in range(5):
+  
+    dato = int(input(f"Captura la calificación {i + 1}: "))
+    
+
+    calificaciones.append(dato)
+
+print("Calificaciones capturadas:", calificaciones)
